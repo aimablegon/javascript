e0028feb840e176c3c89b1580e0b421187906673
Gon <aimablegon@gmail.com>
2022-03-13 23:39:45 +0900

    Fix: phaser chapter 7

--- phaser/app.js
+++ phaser/app.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 var ball;
+var paddle;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.NO_SCALE;
@@ -10,17 +11,26 @@ function preload() {
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#eee';
   game.load.image('ball', './img/ball.png');
+  game.load.image('paddle', 'img/paddle.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(25, 25, 'ball');
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+  ball.anchor.set(0.5);   
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  ball.body.velocity.set(100, 100);
+  ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  paddle.anchor.set(0.5,1);
+  console.log(paddle)
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
 }
 
 function update() {
- 
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width*0.5;
 }
--- phaser/text.js
+++ phaser/text.js
@@ -8,6 +8,8 @@ var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 var ball; // ball 변수 선언
+var paddle; // paddle 변수 선언
+
 
 // 시작전 로드
 function preload() {
@@ -28,8 +30,10 @@ function preload() {
   // canvas의 백그라운드 컬러값을 eee로 바꾼다
 
   game.load.image('ball', 'img/ball.png');
-  // image 로드 image(key, value)
+  // ball image 로드 image(key, value)
 
+  game.load.image('paddle', 'img/paddle.png');
+  // paddle image 로드 image(key, value)
 }
 
 // 로드 후 생성(한번 실행)
@@ -37,15 +41,26 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   // Arcade Physics(물리시스템) 초기화 시키기
 
-  ball = game.add.sprite(50, 50, 'ball');
+  // ball = game.add.sprite(50, 50, 'ball');
   // sprite(x좌표, y좌표, preload함수의 image에서 사용한key값과 같은것을 넣는다 <key:렌더링 되는 동안의텍스처 또는 이미지>)
+  // x좌표: 50px, y좌표:50px에서 시작하는 'ball' 이라는 키워드을 ball변수에 담는다
+
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+  // x좌표: game.world(게임의 캔버스 전체 widht)*0.5 = 480*0.5 = 240
+  // x좌표: game.world(게임의 캔버스 젙체 height)- 25 = 320 - 5 = 295
+  // 240, 295에서 시작하는 'ball' 이라는 키워드을 ball변수에 담는다
+
+  ball.anchor.set(0.5);
+  // ball의 x축 포인터를 0.5로 설정
+  // 마치 패들 위에서 시작하는 듯한 느깜
   
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  // Arcade Physics(물리시스템)으로 공 활성화 시키기
+  // Arcade Physics(물리시스템)으로 변수ball 활성화 시키기
 
-  ball.body.velocity.set(150, 150);
+  // ball.body.velocity.set(150, 150);
   // 공의 속도 움직임 추가(x축, y축) => 한번 움직일때 x축으로 150, y축으로 150
   // 위의 코드 사용시 => update()의 ball.x, ball.y 삭제
+  ball.body.velocity.set(150, -150);
 
   ball.body.collideWorldBounds = true;
   // body의 크기(캔버스의크기)를 벽으로 인식하게 한다 => 단 이것만 쓰면 벽에 두딪힌후 가장자리에서 구른다
@@ -53,6 +68,24 @@ function create() {
   ball.body.bounce.set(1);
   // 가장자리에서 계속 머물지 않고 튕기게 하는 옵션
 
+
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  // 캔버스의 크기가 480,320일 경우
+  // x좌표: game.world(게임의 캔버스 전체 widht)*0.5 = 480*0.5 = 240
+  // x좌표: game.world(게임의 캔버스 젙체 height)- 5 = 320 - 5 = 315
+  // 240픽셀, 315픽셀에서 시작하는 'paddle'이라는 키워드를 paddle 변수에 담는다
+
+  paddle.anchor.set(0.5,1);
+  // paddle의 sprite 위치 설정
+  // 스프라이트의 포인트를 설정합니다. 
+  // 예를 들어 앵커를 0,0으로 설정하면 스프라이트의 왼쪽 위 모서리에 위치 지정이 적용됩니다. 
+  // 앵커를 0.5,0.5로 설정하면 스프라이트 중간에 위치가 적용됩니다.
+
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  // Arcade Physics(물리시스템)으로 변수paddle 활성화 시키기
+
+  paddle.body.immovable = true;
+  // paddle을 immovable(움직이지 않겠다는) 옵션
 }
 
 // 모든 프레임에서 시작
@@ -61,4 +94,14 @@ function update() {
   // // ball의 x 값에 1씩 추가 한다
   // ball.y += 1;
   // // ball의 y 값에 1씩 추가 한다
+
+  game.physics.arcade.collide(ball, paddle);
+  // collide는 충돌감지 함수
+  // ball과 paddle의 충돌 간지를 알고 싶을 때 사용
+
+  // paddle.x = game.input.x;
+  // input으로 마우스, 터치 옵션을 받을 수 있다
+  // input의 x 값을 paddle의 x값에 담는다
+  paddle.x = game.input.x || game.world.width*0.5;
+  // input의 x 값 또는 캔버스의 width값의 50%일 떄 addle의 x값에 담는다
 }
